a16aaa6fc0a999f01e021c25c2201124ef6781bd
fangyunde <fangyunde@inspur.com>
2019-09-19 17:12:46 +0800

    test module

--- shell/randomPassword.sh
+++ shell/randomPassword.sh
@@ -7,14 +7,21 @@ function rand(){
 }
 
 http_post(){    
-	post_data=`curl -X POST -d "$1" http://117.50.43.204:8000/auth/v1/create 2>/dev/null`
-	echo $post_data
+	post_data=`curl -X POST -d "$1" http://117.50.43.204:8000/auth/v1/resetpassword 2>/dev/null`
 	return 1
 }
 rnd=$(rand 100000 999999)
-param="username=13888888888&password=$rnd&expire=24"
-postResult=$(http_post $param)
+old_pas=$(cat /opt/old.pas)
+param1="username=666666&scope=1&old_password=$old_pas&new_password=$rnd"
+param2="username=10001&scope=2&old_password=$old_pas&new_password=$rnd"
+param3="username=201909032&scope=3&old_password=$old_pas&new_password=$rnd"
+postResult=$(http_post $param1)
+postResult=$(http_post $param2)
+postResult=$(http_post $param3)
+echo $rnd > /opt/old.pas
+
 address="http://117.50.43.204"
 echo " address:$address
- student/organization:13888888888 $rnd
- inspur:wait to complete" | mail -v -s "yunpj PassWord Update" -c onlineeval_inspur@163.com 2681666570@qq.com,1019624637@qq.com
+ student: 201909032 $rnd
+ organization: 999999 $rnd
+ inspur: 666666 $rnd" | mail -v -s "yunpj PassWord Update" -c onlineeval_inspur@163.com onlineeval_inspur@163.com
